ef9cc80bd0ea1b735def61c3aaf96349f5761afc
Lu, Youwei <Conan1985@users.noreply.github.com>
2023-09-06 17:53:21 -0500

    Add title to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import {AppBar, Box, Drawer, IconButton, List, ListItem, Toolbar} from "@mui/material";
+import {AppBar, Box, Drawer, IconButton, List, MenuItem, Toolbar} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import RefreshIcon from '@mui/icons-material/Refresh';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import {useState} from "react";
+import UnitedLogo from "../../icons/logo-united.svg";
+import "./header.css";
 
 const Header = () => {
     const [openDrawer, setOpenDrawer] = useState<boolean>(false)
@@ -11,6 +15,7 @@ const Header = () => {
             <Toolbar>
                 <IconButton
                     edge={"start"}
+                    color={"default"}
                     aria-label={"open drawer"}
                     onClick={() => setOpenDrawer(true)}
                 >
@@ -24,13 +29,25 @@ const Header = () => {
                 >
                     <Box>
                         <List>
-                            <ListItem sx={{cursor: "pointer"}} onClick={() => setOpenDrawer(false)}><CloseIcon/></ListItem>
-                            <ListItem>SDR Search function</ListItem>
-                            <ListItem>CPCP Report</ListItem>
-                            <ListItem>Parts Reporting</ListItem>
+                            <MenuItem onClick={() => setOpenDrawer(false)}><CloseIcon/></MenuItem>
+                            <MenuItem>SDR Search function</MenuItem>
+                            <MenuItem>CPCP Report</MenuItem>
+                            <MenuItem>Parts Reporting</MenuItem>
                         </List>
                     </Box>
                 </Drawer>
+                <IconButton>
+                    <img
+                        alt="Logo"
+                        src={UnitedLogo}
+                        className="unitedLogoIcon"
+                    />
+                </IconButton>
+                <header className={"headerTitle"}>
+                    SDR Web
+                </header>
+                <RefreshIcon sx={{marginLeft: "auto", marginRight: "25px"}}/>
+                <PermIdentityIcon sx={{marginRight: "10px"}}/>
             </Toolbar>
         </AppBar>
     );
