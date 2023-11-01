fe175af3f24e1eda270bcffe6a4caefac4b5cd2c
Prakash Apthiri <omprakash.apthiri@united.com>
2023-11-01 13:44:54 -0500

    added extract button and new component to view sdr on the right side

--- src/commons/TabPanel.tsx
+++ src/commons/TabPanel.tsx
@@ -1,26 +1,27 @@
 import { Box } from "@mui/material";
 import { TabPanelProps } from "./types";
 
-
 const TabPanel = (props: TabPanelProps) => {
-    const { children, value, index, ...other } = props;
-    
+  const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${value}`}
-            aria-labelledby={`simple-tab-${value}`}
-            {...other}
-        >
-            {value === index && (
+  return (
+    <div
+      role="tabpanel"
+    //   hidden={value !== index}
+      id={`simple-tabpanel-${value}`}
+      aria-labelledby={`simple-tab-${value}`}
+      {...other}
+    >
+      {/* {value === index && (
                 <Box sx={{ padding: "30px 0 0" }}>
                     <Box>{children}</Box>
                 </Box>
-            )}
-        </div>
-    );
+            )} */}
+      <Box sx={{ padding: "30px 0 0" }}>
+        <Box>{children}</Box>
+      </Box>
+    </div>
+  );
 };
 
 export default TabPanel;
---
+++ src/components/viewsdr/ViewSdrData.tsx
@@ -0,0 +1,22 @@
+import { Box, Grid } from "@mui/material";
+import { ViewSdrDataProps } from "src/commons/types";
+
+const ViewSdrData = (props: ViewSdrDataProps) => {
+    console.log(props.selectedSdrId);
+
+    const sxBox = {
+        borderBottom: 1, 
+        borderColor: 'divider',
+    }
+
+    return (
+      <Grid item md={6}>
+        <Box sx={{ ...sxBox }}>
+            <p>Service Difficulty Report - #{props.selectedSdrId}</p>
+        </Box>
+
+      </Grid>
+    );
+};
+
+export default ViewSdrData;
