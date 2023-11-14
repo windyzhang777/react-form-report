06d826e86fb4f0192e74a2d6a57b075530bdbd7a
Lu, Youwei <youwei.lu@united.com>
2023-11-14 13:52:14 -0600

    1329518 Add operator control number and a/c information header

--- src/components/viewsdr/ViewSdrData.tsx
+++ src/components/viewsdr/ViewSdrData.tsx
@@ -1,14 +1,17 @@
-import { Box, Grid } from "@mui/material";
+import {Box, Grid, ListItem} from "@mui/material";
 import { ViewSdrDataProps } from "src/commons/types";
+import "./viewSdrData.css";
 
 const ViewSdrData = (props: ViewSdrDataProps) => {
-    console.log(props.selectedSdrId);
+
+    const opeartorControlumber = "34564567320230714";
 
     const sxBox = {
         borderBottom: 1, 
         borderColor: 'divider',
-        width: '195%',
-        fontWeight: '600'
+        width: '190%',
+        fontWeight: '600',
+        marginLeft: '15px'
     }
 
     return (
@@ -16,6 +19,26 @@ const ViewSdrData = (props: ViewSdrDataProps) => {
         <Box sx={{ ...sxBox }}>
             <p>Service Difficulty Report - #{props.selectedSdrId}</p>
         </Box>
+          <Grid container spacing={2}
+                sx={{marginTop: "10px", color: "#666666", fontWeight: 400}}>
+              <Grid item xs={6}>
+                  <ListItem>Operator Control Number</ListItem>
+              </Grid>
+              <Grid item xs={6}>
+                  <ListItem>A/C Information</ListItem>
+              </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+              <Grid item xs={6}>
+                  <ListItem>{opeartorControlumber}</ListItem>
+              </Grid>
+              <Grid item xs={6}>
+                  <ListItem> <u className={"view-details-text"}>View Details</u></ListItem>
+              </Grid>
+          </Grid>
+
+
+
           {props.selectedSdrId === null && <Grid>Please select on SDR to view it.</Grid>}
       </Grid>
     );
