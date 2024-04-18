7866164fd2cb81d235212a49bee28f345557884f
Zhang, Windy <Windy.Zhang@united.com>
2024-04-18 13:04:17 -0500

    Bug 1535751: [OBSERVATION][WEB] eSFR: Inconsistency Between User Story and UI -Difficulty Date Format Mismatch

--- src/components/viewsdr/ViewSdrData.tsx
+++ src/components/viewsdr/ViewSdrData.tsx
@@ -18,7 +18,7 @@ import {
   Sides,
   UserPermission,
 } from "src/commons/types";
-import { DATETIME_DISPLAY, DATETIME_REQUEST, DATE_HTML_DISPLAY, toFixed } from "src/helpers";
+import { DATETIME_REQUEST, DATE_DISPLAY, DATE_HTML_DISPLAY, toFixed } from "src/helpers";
 import { useAppSelector } from "src/redux/hooks";
 import "./viewSdrData.css";
 
@@ -340,7 +340,7 @@ const ViewSdrData = ({
                             className={"sdr-status-edit"}
                           />
                         ) : (
-                          moment(values?.LogPageCreationDate).format(DATETIME_DISPLAY)
+                          moment(values?.LogPageCreationDate).format(DATE_DISPLAY)
                         )}
                       </ListItem>
                     </Grid>
